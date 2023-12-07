5831af1b7bec4a0f4d3444350efea5b77fcd7c4f
JordyWeltensPXL <92799790+JordyWeltensPXL@users.noreply.github.com>
2023-12-07 21:19:35 +0100

    project setup, vue router, sass, pinia

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navigation from './components/Navigation.vue'
+</script>
+
+<template>
+  <Navigation></Navigation>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navigation">
+        <RouterLink to="/" class="navigation__logo">Logo</RouterLink>
+
+        <div class="navigation links">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/">Products</RouterLink>
+          <RouterLink to="/">About us</RouterLink>
+          <RouterLink to="/">Login</RouterLink>
+          <span>cart icon</span>
+        </div>
+        
+      </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
