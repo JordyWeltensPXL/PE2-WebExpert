e9fcecfd4d31b79ab6c963616b97bce0c8aa483c
JordyWeltensPXL <92799790+JordyWeltensPXL@users.noreply.github.com>
2023-12-17 12:03:58 +0100

    added functionality to filtering, just missing select x items on the page

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,51 +1,100 @@
 <template>
     <div>
-      <div class="products__filters">
-        <p>Filter:</p>
-        <button class="products__filters__button" @click="filterByStock">&lt; 40 in stock</button>
-        <button class="products__filters__button" @click="filterByPrice">&lt; € 20</button>
-      </div>
-      <div v-for="album in displayedAlbums" :key="album.id" class="productCard">
-        <RouterLink to="/">
-          <img class="productCard__image" :src="album.src" alt="">
-        </RouterLink>
-        <div class="productCard__info">
-          <p class="productCard__info__headline">{{ album.title }}</p>
-          <div class="productCard__info__buy">
-            <p class="productCard__info__buy__price">€ {{ album.price }}</p>
-            <button class="productCard__info__buy__button">Add to cart</button>
+        <div class="filters">
+            <p>Filter:</p>
+            <button class="filters__button" @click="toggleFilter('stock')">&lt; 40 in stock</button>
+            <button class="filters__button" @click="toggleFilter('price')">&lt; € 20</button>
           </div>
-        </div>
+        <div class="wrapper">
+            <div v-for="album in displayedAlbums" :key="album.id" class="productCard">
+              <RouterLink to="/">
+                <img class="productCard__image" :src="album.src" alt="">
+              </RouterLink>
+              <div class="productCard__info">
+                <p class="productCard__info__headline">{{ album.title }}</p>
+                <div class="productCard__info__buy">
+                  <p class="productCard__info__buy__price">€ {{ album.price }}</p>
+                  <button class="productCard__info__buy__button">Add to cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+      <div class="pagination">
+        <button class="pagination__button" @click="prevPage" :disabled="currentPage === 1">Previous</button>
+        <span>Page {{ currentPage }} of {{ totalPages }}</span>
+        <button class="pagination__button" @click="nextPage" :disabled="currentPage === totalPages">Next</button>
       </div>
     </div>
+
+    <div class="items-per-page">
+        <label for="itemsPerPage">Items per page:</label>
+        <select v-model="itemsPerPage" @change="handleItemsPerPageChange" id="itemsPerPage">
+          <option value="8">8</option>
+          <option value="16">16</option>
+          <option value="24">24</option>
+        </select>
+      </div>
   </template>
   
   <script>
-  import albumsData from "@/albums.json";
-  
-  export default {
-    data() {
-      return {
-        albums: albumsData,
-        displayedAlbums: albumsData, // Initially set displayedAlbums to all albums
-      };
-    },
-    methods: {
-      filterByStock() {
-        // Filtering albums with stock less than 40
-        this.displayedAlbums = this.albums.filter((album) => album.stock < 40);
+import albumsData from "@/albums.json";
+
+export default {
+  data() {
+    return {
+      albums: albumsData,
+      filters: {
+        stock: false,
+        price: false,
       },
-      filterByPrice() {
-        // Filtering albums with price less than 20
-        this.displayedAlbums = this.albums.filter(
-          (album) => parseFloat(album.price.replace(",", ".")) < 20
-        );
-      },
+      itemsPerPage: 8,
+      currentPage: 1,
+    };
+  },
+  computed: {
+    filteredAlbums() {
+      return this.albums.filter((album) => {
+        const meetsStockCriteria = !this.filters.stock || album.stock < 40;
+        const meetsPriceCriteria =
+          !this.filters.price || parseFloat(album.price.replace(",", ".")) < 20;
+
+        return meetsStockCriteria && meetsPriceCriteria;
+      });
     },
-  };
-  </script>
+    displayedAlbums() {
+  const start = (this.currentPage - 1) * this.itemsPerPage;
+  const end = start + this.itemsPerPage;
+
+  return this.filteredAlbums.slice(start, end);
+},
+totalPages() {
+  return Math.ceil(this.filteredAlbums.length / this.itemsPerPage);
+},
+  },
+  methods: {
+    toggleFilter(filterType) {
+      this.filters[filterType] = !this.filters[filterType];
+      this.currentPage = 1;
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    handleItemsPerPageChange() {
+    this.currentPage = 1; // Reset to the first page when items per page changes
+    },
+  },
+};
+</script>
+
   
   <style>
-    /* Add your styles here */
   </style>
   
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -10,23 +10,6 @@ import ProductCard from '../components/ProductCard.vue'
       <ul class="products__productCards">
         <li class="products__productCards__card"><ProductCard></ProductCard></li>
       </ul>
-      <ul class="products__pages">
-        <div class="products__pages__items">
-          <p class="products__pages__items__text">Items per page</p>
-          <select class="products__pages__items__select" name="itemsPerPage" id="itemsPerPage">
-            <option value="8">8</option>
-            <option value="16">16</option>
-            <option value="24">24</option>
-          </select>
-        </div>
-        
-        <div class="products__pages__list">
-          <li class="products__pages____list__listItem"><button class="products__pages__list__listItem__button">1</button></li>
-          <li class="products__pages__list__listItem"><button class="products__pages__list__listItem__button">2</button></li>
-          <li class="products__pages__list__listItem"><button class="products__pages__list__listItem__button">3</button></li>
-          <li class="products__pages__list__listItem"><button class="products__pages__list__listItem__button">4</button></li>  
-        </div>
-      </ul>
     </div>
 </template>
 
