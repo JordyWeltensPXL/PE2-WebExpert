d245a775164f9dd20ebf501ea850a0101228a406
JordyWeltensPXL <92799790+JordyWeltensPXL@users.noreply.github.com>
2023-12-19 17:19:59 +0100

    shopping cart fully functional, small bug when ordering 5 of the same albums to be fixed, starting on checkoutpage

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,36 +1,84 @@
 <template>
-    <div>
-      <h2>Shopping Cart</h2>
-      <div v-if="cart.length === 0">
-        <p>Your cart is empty.</p>
-      </div>
-      <div v-else>
-        <div v-for="cartItem in cart" :key="cartItem.album.id" class="cart-item">
-          <p>{{ cartItem.album.title }} - Quantity: {{ cartItem.quantity }}</p>
-          <button @click="removeFromCart(cartItem.album.id)">Remove</button>
-        </div>
-        <p>Total: € {{ total }}</p>
-        <button @click="clearCart">Clear Cart</button>
-      </div>
+  <div class="cart">
+    <h2 class="cart__headline">Shopping Cart</h2>
+    <div class="cart__empty" v-if="cart.length === 0">
+      <p>Your cart is empty.</p>
     </div>
-  </template>
+    <div class="cart__content" v-else>
+      <div class="cart__content-item" v-for="cartItem in cart" :key="cartItem.album.id">
+        <div class="cart__content-item-wrapper">
+          <img class="cart__content-item-wrapper-image" :src="cartItem.album.src" alt="">
+          <div class="cart__content-item-wrapper-info-amount">
+            <p class="cart__content-item-title">{{ cartItem.album.title }}</p>
+            <div class="cart__content-wrapper">
+              <button class="cart__content-button" @click="decrementQuantity(cartItem)">-</button>
+              <span class="cart__content-item-quantity">{{ cartItem.quantity }}</span>
+              <button class="cart__content-button" @click="incrementQuantity(cartItem)">+</button>
+            </div>
+            <p class="cart__content-item-price">Price: € {{ cartItem.album.price }} each</p>
+            <p class="cart__content-item-VAT">VAT ({{ cartItem.album.VAT }}%): € {{ calculateVAT(cartItem.album.price, cartItem.album.VAT) }}</p>
+            <p class="cart__content-item-subtotal">Subtotal (per item): € {{ calculateSubtotal(cartItem.album.price, cartItem.quantity) }}</p>
+            <button class="cart__content-item-removeButton" @click="removeFromCart(cartItem.album.id)">Remove item</button>
+          </div>
+        </div>
+      </div>
+      <p class="cart__totalPrice">Total: € {{ formattedTotal }}</p>
+      <div class="cart__buttonsWrapper">
+        <button class="cart__orderButton">Order Now</button>
+        <button class="cart__clearButton" @click="clearCart">Clear Shopping Cart</button>
+      </div>
+      
+    </div>
+  </div>
+</template>
   
-  <script>
-  import { useCartStore } from '@/stores/cart';
-  
-  export default {
-    setup() {
-      const cartStore = useCartStore();
-  
-      return {
-        cart: cartStore.cart,
-        total: cartStore.total,
-        removeFromCart: cartStore.removeFromCart,
-        clearCart: cartStore.clearCart,
-      };
-    },
-  };
-  </script>
+<script>
+import { useCartStore } from '@/stores/cart';
+
+export default {
+  setup() {
+    const cartStore = useCartStore();
+
+    const calculateVAT = (price, VAT) => {
+      const vatAmount = (price * VAT) / 100;
+      return parseFloat(vatAmount.toFixed(2));
+    };
+
+    const calculateSubtotal = (price, quantity) => {
+      return price * quantity;
+    };
+
+    const calculateTotalVAT = () => {
+      return cart.reduce((total, cartItem) => {
+        return total + calculateVAT(cartItem.album.price, cartItem.album.VAT) * cartItem.quantity;
+      }, 0).toFixed(2);
+    };
+
+    const incrementQuantity = (cartItem) => {
+      cartItem.quantity += 1;
+    };
+
+    const decrementQuantity = (cartItem) => {
+      if (cartItem.quantity > 1) {
+        cartItem.quantity -= 1;
+      }
+    };
+
+    return {
+      cart: cartStore.cart,
+      removeFromCart: cartStore.removeFromCart,
+      clearCart: cartStore.clearCart,
+      calculateVAT,
+      calculateSubtotal,
+      calculateTotalVAT,
+      incrementQuantity,
+      decrementQuantity,
+      formattedTotal: cartStore.formattedTotal
+    };
+  },
+};
+</script>
+
 
 <style>
 
